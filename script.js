1a8b651db89417ea6b1d33800e964ca0f2a0083d
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-07-21 18:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function fibonacci(num) {
 // your code here
-	    if (num <= 1) {
-        return num;
+	if (num<=0){
+		throw new ERROR("Input must be positive integer")
+	}
+	    if (num == 1) {
+        return 0;
     }
+	    if (num == 2){
+			return 1;
+		}
     
     let fibPrev = 0;
     let fibCurr = 1;
