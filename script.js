a98f2b54fc3e68a86a1a0d09192d0680bbde29e5
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-07-21 18:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function fibonacci(num) {
 // your code here
-	if (n<2>){
-		return n
-	}
-	return fibonacci(n-1) + fibonacci(n-2)
+	    if (num <= 1) {
+        return num;
+    }
+    
+    let fibPrev = 0;
+    let fibCurr = 1;
+    let fibNext;
+    
+    for (let i = 2; i <= num; i++) {
+        fibNext = fibPrev + fibCurr;
+        fibPrev = fibCurr;
+        fibCurr = fibNext;
+    }
+    
+    return fibCurr;
+	
 }
 
 module.exports = fibonacci;
